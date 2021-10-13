4b232d9e9fc00e1d512ce65457530926e990fe23
GajendraAchar <92097176+GajendraAchar@users.noreply.github.com>
2021-10-13 12:57:34 +0530

    Update usersSort.java

--- src/main/java/usersSort.java
+++ src/main/java/usersSort.java
@@ -87,7 +87,7 @@ public class usersSort {
 
   @AfterClass(alwaysRun = true)
   public void tearDown() throws Exception {
-    driver.quit();
+    //driver.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
       fail(verificationErrorString);
