442beb759a42e8b137f8c5ab08128b55bedc8f43
GajendraAchar <92097176+GajendraAchar@users.noreply.github.com>
2021-10-13 12:58:18 +0530

    Update addUser.java

--- src/main/java/addUser.java
+++ src/main/java/addUser.java
@@ -194,7 +194,7 @@ public class addUser {
 
 	@AfterClass(alwaysRun = true)
 	public void tearDown() throws Exception {
-		driver.quit();
+		//driver.quit();
 		String verificationErrorString = verificationErrors.toString();
 		if (!"".equals(verificationErrorString)) {
 			fail(verificationErrorString);
